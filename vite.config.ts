7a1ebe2ff467f1a34d963d5b01cda6f5db4b64e1
lingan7 <muthulingakalyan@gmail.com>
2025-11-26 00:10:13 -0500

    Base URL change

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,26 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/KM_Medical/',
+  base: '/',
   server: {
     port: 5173,
     open: true
-  }
+  },
+  build: {
+    assetsDir: 'assets',
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 });
